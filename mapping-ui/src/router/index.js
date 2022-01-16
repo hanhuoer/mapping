a674a52822ff274b3c1e2aee26955c4aaa046c2e
hanhuoer <1206751678@qq.com>
2022-01-16 14:28:23 +0800

    fix: Avoided redundant navigation to current location.

--- mapping-ui/src/router/index.js
+++ mapping-ui/src/router/index.js
@@ -2,35 +2,40 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Layout from '@/layout/Index'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
+
+const originalPush = VueRouter.prototype.push;
+VueRouter.prototype.push = function push(location) {
+    return originalPush.call(this, location).catch(err => err)
+};
 
 export const constantRoutes = [
-  {
-    path: '/login',
-    component: () => import('../views/Login')
-  },
-  {
-    path: '/',
-    component: Layout,
-    children: [
-      {
+    {
+        path: '/login',
+        component: () => import('../views/Login')
+    },
+    {
         path: '/',
-        component: () => import('@/views/Home')
-      },
-      {
-        path: '/about',
-        component: () => import('@/views/About')
-      },
-      {
-        path: '/client',
-        component: () => import('@/views/Client')
-      }
-    ]
-  }
-]
+        component: Layout,
+        children: [
+            {
+                path: '/',
+                component: () => import('@/views/Home')
+            },
+            {
+                path: '/about',
+                component: () => import('@/views/About')
+            },
+            {
+                path: '/client',
+                component: () => import('@/views/Client')
+            }
+        ]
+    }
+];
 
 const router = new VueRouter({
-  routes: constantRoutes
-})
+    routes: constantRoutes
+});
 
 export default router
