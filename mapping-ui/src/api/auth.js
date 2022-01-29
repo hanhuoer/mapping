4d188b26807c38266b062d279ba83f0c835c9229
hanhuoer <1206751678@qq.com>
2022-01-29 22:02:22 +0800

    add refresh.

--- mapping-ui/src/api/auth.js
+++ mapping-ui/src/api/auth.js
@@ -15,3 +15,24 @@ export function signIn(username, password) {
         data: formData
     })
 }
+
+export async function refresh() {
+    return request({
+        url: '/auth/refresh',
+        method: 'post'
+    })
+}
+
+export function getRefreshLeastSeconds() {
+    return request({
+        url: '/auth/get/refresh/least/seconds',
+        method: 'get'
+    })
+}
+
+export function getExpireAt() {
+    return request({
+        url: '/auth/get/expire/at',
+        method: 'get'
+    })
+}
--- mapping-ui/src/layout/Header.vue
+++ mapping-ui/src/layout/Header.vue
@@ -12,8 +12,107 @@
 </template>
 
 <script>
+    import {getToken, setToken} from "../utils/auth";
+    import {getExpireAt, getRefreshLeastSeconds, refresh} from "../api/auth";
+
     export default {
-        name: "Header"
+        name: "Header",
+        data() {
+            return {
+                defaultRefreshLessSeconds: 120,
+                serverRefreshLessSeconds: null,
+                lastTokenExpireAt: null,
+                getServerRefreshLessSecondsFailedTimes: 0,
+                getLastTokenExpireAtFailedTimes: 0,
+                doRefreshLock: true
+            }
+        },
+        created() {
+            this.getTokenExpireAt();
+            this.getServerRefreshLessSeconds();
+            setTimeout(() => {
+                this.doRefresh();
+            }, 10000);
+        },
+        methods: {
+            doRefresh: function () {
+                setTimeout(() => {
+                    let currentToken = getToken();
+                    if (currentToken === undefined || currentToken === null || currentToken === "") {
+                        this.$router.push("/login");
+                        return;
+                    }
+                    if (this.doRefreshLock) {
+                        this.doRefreshLock = false;
+                        if (this.checkRefreshTiming()) {
+                            this.log.debug("start refreshing the token");
+                            refresh().then(response => {
+                                if (response.code === "20000") {
+                                    let token = response.data;
+                                    this.log.debug(`the new token: ${token}`);
+                                    if (token !== null && token !== "") {
+                                        setToken(token);
+                                        this.log.debug(`save the new token`);
+                                        this.getTokenExpireAt();
+                                        this.getServerRefreshLessSeconds();
+                                    }
+                                }
+                                this.doRefreshLock = true;
+                                this.doRefresh();
+                            }).catch(error => {
+                                this.doRefreshLock = true;
+                                this.doRefresh();
+                            });
+                        } else {
+                            this.log.debug("do not need refresh the token");
+                            this.doRefreshLock = true;
+                            this.doRefresh();
+                        }
+                    }
+                }, 10000);
+            },
+            checkRefreshTiming: function () {
+                let now = new Date();
+                let expireAt = this.lastTokenExpireAt;
+                if (expireAt === null || expireAt <= 0) {
+                    if (this.getLastTokenExpireAtFailedTimes < 5) {
+                        this.getTokenExpireAt();
+                        this.getLastTokenExpireAtFailedTimes += 1;
+                    }
+                    return false;
+                }
+                if (expireAt > now.getTime()) {
+                    let lessSeconds = this.serverRefreshLessSeconds;
+                    if (lessSeconds === null) {
+                        if (lessSeconds <= 0) {
+                            this.getServerRefreshLessSecondsFailedTimes = 6;
+                        }
+                        lessSeconds = this.defaultRefreshLessSeconds;
+
+                        if (this.getServerRefreshLessSecondsFailedTimes < 5) {
+                            this.getServerRefreshLessSeconds();
+                            this.getServerRefreshLessSecondsFailedTimes += 1;
+                        }
+                    }
+                    let expireSeconds = (expireAt - now.getTime()) / 1000;
+                    this.log.debug(`expires in ${expireSeconds} seconds, refresh the token ${lessSeconds} seconds before expiration`);
+                    return expireSeconds < lessSeconds;
+                }
+                return false;
+            },
+            getTokenExpireAt: function () {
+                getExpireAt().then(response => {
+                    this.lastTokenExpireAt = response.data;
+                    this.log.debug(`expiration time at: ${new Date(response.data).toISOString()}, timestamp: ${response.data}`);
+                })
+            },
+            getServerRefreshLessSeconds: function () {
+                getRefreshLeastSeconds().then(response => {
+                    this.serverRefreshLessSeconds = response.data;
+                    this.log.debug(`minimum seconds refresh: ${response.data}`);
+                })
+            }
+        }
     }
 </script>
 
